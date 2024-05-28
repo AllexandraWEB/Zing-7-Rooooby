d9e503763e00e56626e5485c6555a40341276225
Alexandra <cimentarowa@gmail.com>
2024-05-28 15:55:29 +0300

    Updating an error

--- src/components/layout/Benefits.tsx
+++ src/components/layout/Benefits.tsx
@@ -6,7 +6,7 @@ const Benefits = () => {
     <section className='w-full h-[1750px] md:h-[879px] bg-primary-blue text-white'>
       <div className='regular-container h-full flex flex-wrap'>
         <div className='w-[613px] h-[116px] mt-[90px] md:mt-[120px]'>
-          <h1 className='text-[30px] md:text-[48px] font-bold text-center md:text-start mx-1'>Here's how Roooby can benefit your business</h1>
+          <h1 className='text-[30px] md:text-[48px] font-bold text-center md:text-start mx-1'>Here&#39;s how Roooby can benefit your business</h1>
         </div>
         <div className='w-[487px] h-[84px] mt-[5px] md:mt-[156px]'>
           <p className='text-[16px] md:text-[18px] text-center md:text-start mx-1'>Build more meaningful and lasting relationships — better understand their needs, identify new opportunities to help, address any problems faster.</p>
